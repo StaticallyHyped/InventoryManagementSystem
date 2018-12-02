1b00ac4288199fabcd12ec12aea1e155797d1258
Chris <45054064+StaticallyHyped@users.noreply.github.com>
2018-12-02 15:30:24 -0800

    All UI Buttons working
    - All UI buttons working, now need to make save buttons populate data
    - The different methods that I've used to switch between the screens are cumbersome, at best. I'm assuming there's a better way to do this - will come back and refactor once I've got the thing working.

--- src/software1/PartController.java
+++ src/software1/PartController.java
@@ -5,12 +5,9 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -39,18 +36,25 @@ public class PartController {
     private Button addProductCancel;
     @FXML
     private Button modProductCancel;
+    @FXML
+    private RadioButton goToAddPartOutButton;
+    @FXML
+    private RadioButton goToAddPartInButton;
+    @FXML
+    private Button addPartOutCancel;
+    @FXML
+    private RadioButton goToModPartOutButton;
+    @FXML
+    private RadioButton goToModPartInButton;
+    @FXML
+    private Button modPartOutCancel;
 
-//    public ObservableList<PartInhouse> addPartInData = FXCollections.observableArrayList();
 
-//    public PartController() {
-//        this.addPartInData.add(new PartInhouse("14", "Musterd", "test",
-//                "also test", 1, 2,45));
-//
-//    }
     @FXML
     private void modPartInBackToMain(ActionEvent event) throws IOException{
 
         Stage stage = (Stage) modPartInCancel.getScene().getWindow();
+
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainScreen.fxml"));
         AnchorPane page = loader.load();
         Scene scene = new Scene(page);
@@ -87,7 +91,65 @@ public class PartController {
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    private void goToAddPartOutsourced(ActionEvent event) throws IOException{
 
+        Stage stage = (Stage) goToAddPartOutButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("addPartOut.fxml"));
+        AnchorPane page = loader.load();
+        Scene scene = new Scene(page);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    private void goToAddPartIn(ActionEvent event) throws IOException{
+
+        Stage stage = (Stage) goToAddPartInButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("addPartIn.fxml"));
+        AnchorPane page = loader.load();
+        Scene scene = new Scene(page);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    private void addPartOutBackToMain(ActionEvent event) throws IOException{
+
+        Stage stage = (Stage) addPartOutCancel.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainScreen.fxml"));
+        AnchorPane page = loader.load();
+        Scene scene = new Scene(page);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    private void goToModPartOutsourced(ActionEvent event) throws IOException{
+
+        Stage stage = (Stage) goToModPartOutButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("ModPartOut.fxml"));
+        AnchorPane page = loader.load();
+        Scene scene = new Scene(page);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    private void goToModPartIn(ActionEvent event) throws IOException{
+        Stage stage = (Stage) goToModPartInButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("ModPartIn.fxml"));
+        AnchorPane page = loader.load();
+        Scene scene = new Scene(page);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    private void modPartOutBackToMain(ActionEvent event) throws IOException{
+
+        Stage stage = (Stage) modPartOutCancel.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainScreen.fxml"));
+        AnchorPane page = loader.load();
+        Scene scene = new Scene(page);
+        stage.setScene(scene);
+        stage.show();
+    }
     public void setAddPartID(TextField addPartID) {
         this.addPartID = addPartID;
     }
